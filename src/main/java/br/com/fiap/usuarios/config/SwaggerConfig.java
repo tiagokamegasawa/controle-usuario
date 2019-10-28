dc8ec10de8d038bb8f43e34f196a4acf5b1d36a5
Logon Aluno <logonaluno@local.com>
2019-10-28 20:47:56 -0300

    Swagger funcionando p0##@

--- src/main/java/br/com/fiap/usuarios/config/SwaggerConfig.java
+++ src/main/java/br/com/fiap/usuarios/config/SwaggerConfig.java
@@ -1,10 +1,11 @@
 package br.com.fiap.usuarios.config;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -15,7 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig extends WebMvcConfigurationSupport {
+public class SwaggerConfig implements WebMvcConfigurer {
 
   @Bean
   public Docket api() {
@@ -40,7 +41,7 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
   }
 
   @Override
-  protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
     registry.addResourceHandler("swagger-ui.html")
         .addResourceLocations("classpath:/META-INF/resources/");
 
